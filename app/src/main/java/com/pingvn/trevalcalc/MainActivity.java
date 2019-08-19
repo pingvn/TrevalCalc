ff9c651badf84339906be51c8b1ef5402b60d542
Kirill <pingvn@gmail.com>
2019-08-19 11:53:53 +0300

    rebranding

--- app/src/main/java/com/pingvn/trevalcalc/MainActivity.java
+++ app/src/main/java/com/pingvn/trevalcalc/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.CollapsibleActionView;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.pingvn.trevalcalc.Fragments.AddDirectionFragment;
 import com.pingvn.trevalcalc.Fragments.AddTouristFragment;
 import com.pingvn.trevalcalc.Fragments.AddTrevalFragment;
@@ -32,15 +37,23 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         EditDirectionFragment.OnEditeDirectionFragmentInteractionListener {
     private String mEditeTrevalName = " ";
     private Toolbar mToolbar;
+    private CollapsingToolbarLayout mCollapsindTool;
+    private AppBarLayout mAppBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCollapsindTool = findViewById(R.id.toolbar_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mAppBarLayout = findViewById(R.id.app_bar);
         setSupportActionBar(mToolbar);
+        mCollapsindTool.setExpandedTitleGravity(Gravity.CENTER_HORIZONTAL);
+        mCollapsindTool.setExpandedTitleTextAppearance(R.style.AppBarExpanded);
+        mCollapsindTool.setCollapsedTitleTextAppearance(R.style.AppBarCollapsed);
         Realm.init(this);
         getSupportFragmentManager().beginTransaction().add(R.id.id_frame_container, new MainFragmentView()).commit();
+
 
     }
 
@@ -50,10 +63,12 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         switch (mTrevalName) {
             case "#createtreval": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTrevalFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#edit": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditTrevalFragment.newInstance(mName)).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
         }
@@ -65,18 +80,22 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         switch (respone) {
             case "#createDirection": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddDirectionFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#createTourist": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTouristFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#create": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
+                mAppBarLayout.setExpanded(true);
             }
             break;
             case "#cancel": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
+                mAppBarLayout.setExpanded(true);
             }
             break;
         }
@@ -89,16 +108,20 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
             case "#create": {
                 if (mEditeTrevalName == " ") {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTrevalFragment()).commit();
+                    mAppBarLayout.setExpanded(false);
                 } else {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditTrevalFragment.newInstance(mEditeTrevalName)).commit();
+                    mAppBarLayout.setExpanded(false);
                 }
             }
             break;
             case "#cancel": {
                 if (mEditeTrevalName == " ") {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTrevalFragment()).commit();
+                    mAppBarLayout.setExpanded(false);
                 } else {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditTrevalFragment.newInstance(mEditeTrevalName)).commit();
+                    mAppBarLayout.setExpanded(false);
                 }
             }
             break;
@@ -112,8 +135,10 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
             case "#create": {
                 if (mEditeTrevalName == " ") {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTrevalFragment()).commit();
+                    mAppBarLayout.setExpanded(false);
                 } else {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditTrevalFragment.newInstance(mEditeTrevalName)).commit();
+                    mAppBarLayout.setExpanded(false);
                 }
 
             }
@@ -121,8 +146,10 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
             case "#cancel": {
                 if (mEditeTrevalName == " ") {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTrevalFragment()).commit();
+                    mAppBarLayout.setExpanded(false);
                 } else {
                     getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditTrevalFragment.newInstance(mEditeTrevalName)).commit();
+                    mAppBarLayout.setExpanded(false);
                 }
             }
             break;
@@ -135,20 +162,24 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         switch (mCommand) {
             case "#createDirection": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddDirectionFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#createTourist": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new AddTouristFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#create": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
                 mEditeTrevalName = " ";
+                mAppBarLayout.setExpanded(true);
             }
             break;
             case "#cancel": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
                 mEditeTrevalName = " ";
+                mAppBarLayout.setExpanded(true);
             }
             break;
         }
@@ -166,14 +197,17 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         switch (item.getItemId()) {
             case R.id.id_menu_edit_turist: {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditListsTuristandDirectionFragment.newInstance(EditListsTuristandDirectionFragment.TOURIST_CHOISE)).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case R.id.id_menu_edit_direction: {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditListsTuristandDirectionFragment.newInstance(EditListsTuristandDirectionFragment.DIRECTION_CHOISE)).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case R.id.id_menu_delete_all: {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new DeleteAllFragment()).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
         }
@@ -183,6 +217,7 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
     @Override
     public void onDeleteAllFragmentInteraction() {
         getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
+        mAppBarLayout.setExpanded(true);
     }
 
     @Override
@@ -190,10 +225,12 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
         switch (mCommand) {
             case "#editDirection": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditDirectionFragment.newInstance(mName)).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
             case "#editeTourist": {
                 getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, EditeTouristFragment.newInstance(mName)).commit();
+                mAppBarLayout.setExpanded(false);
             }
             break;
 
@@ -204,12 +241,12 @@ public class MainActivity extends AppCompatActivity implements MainFragmentView.
     @Override
     public void onEditeDirectionFragmentInteraction(String message) {
         getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
-
+        mAppBarLayout.setExpanded(true);
     }
 
     @Override
     public void onEditeTouristFragmentInteraction(String message) {
         getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_container, new MainFragmentView()).commit();
-
+        mAppBarLayout.setExpanded(true);
     }
 }
